c3d7a952d2afa1e78fd43bcd589ac7be0aeebd25
chengyang.chen <chengyang.chen@tophant.com>
2020-12-10 16:52:32 +0800

    init

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Application
+ * @Description //TODO
+ * @Author ccy
+ * @Date 2020/12/10 16:50
+ * @Version 1.0
+ **/
+public class Application {
+    public static void main(String[] args){
+        System.out.println("1111");
+    }
+}
